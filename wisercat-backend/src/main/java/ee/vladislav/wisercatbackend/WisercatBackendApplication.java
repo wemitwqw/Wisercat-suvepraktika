6ca8c642d59df7511ce0a7cb60d5b62eb99be13c
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2022-12-20 17:57:28 +0200

    upd

---
+++ wisercat-backend/src/main/java/ee/vladislav/wisercatbackend/WisercatBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.vladislav.wisercatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisercatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WisercatBackendApplication.class, args);
+    }
+
+}
---
+++ wisercat-backend/src/test/java/ee/vladislav/wisercatbackend/WisercatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.vladislav.wisercatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisercatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
